093a2ad8ec333e0cf9417b132ef0d415c57bd9bd
jmn <norlund@gmail.com>
2021-01-03 14:09:54 +0100

    Auth Cleanup

--- src/auth.rs
+++ src/auth.rs
@@ -5,7 +5,7 @@ use {
 };
 // Alternatively, this can be oauth2::curl::http_client or a custom.
 use oauth2::reqwest::http_client;
-use oauth2::{AuthorizationCode, CsrfToken, Scope};
+use oauth2::{AuthorizationCode, CsrfToken, Scope, TokenResponse};
 use tide::log::info;
 
 #[derive(Debug, Deserialize)]
@@ -51,51 +51,16 @@ pub(super) async fn login_authorized(req: Request) -> Result {
 
     info!("Google returned the following token:\n{:?}\n", token_result);
 
-    // let access_token = token_res.access_token();
-    // info!("token_result: %{:#?}", &token_result);
-    // info!("token_res: {:#?}", serde_json::from_str(&token_res.into()));
-
-    // info!("CHECKING TOKEN");
-    // match &token_res {
-    //     Ok(token) => {
-    //         let access_token = token.access_token();
-
-    //         // NB: Github returns a single comma-separated "scope" parameter instead of multiple
-    //         // space-separated scopes. Github-specific clients can parse this scope into
-    //         // multiple scopes by splitting at the commas. Note that it's not safe for the
-    //         // library to do this by default because RFC 6749 allows scopes to contain commas.
-    //         let scopes = if let Some(scopes_vec) = token.scopes() {
-    //             scopes_vec
-    //                 .iter()
-    //                 .map(|comma_separated| comma_separated.split(','))
-    //                 .flatten()
-    //                 .collect::<Vec<_>>()
-    //         } else {
-    //             Vec::new()
-    //         };
-    //         info!("Github returned the following scopes:\n{:?}\n", scopes);
-
-    //         // let userinfo: UserInfoResponse =
-    //         //     surf::get("https://www.googleapis.com/oauth2/v2/userinfo")
-    //         //         .header(
-    //         //             http_types::headers::AUTHORIZATION,
-    //         //             format!("Bearer {}", token.access_token().secret()),
-    //         //         )
-    //         //         .recv_json()
-    //         //         .await?;
-    //     }
-    //     Err(e) => {
-    //         info!("TOKEN FAILED: {:?} {:?} ", e, token_res)
-    //     }
-    // }
-
-    // if let Ok(token) = token_res {
-
-    //     //return userinfo;
-    // }
+    let userinfo: UserInfoResponse = surf::get("https://www.googleapis.com/oauth2/v2/userinfo")
+        .header(
+            http_types::headers::AUTHORIZATION,
+            format!("Bearer {}", token_result?.access_token().secret()),
+        )
+        .recv_json()
+        .await?;
 
     let session = Session {
-        email: String::from("Foo@bar.com"), //userinfo.email,
+        email: String::from(userinfo.email)
     };
 
     let resp: Response = Redirect::new("/").into();
